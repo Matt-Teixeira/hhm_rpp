13e7245cd272f1075c7e81b3d84a61e7753c8fc6
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2023-08-01 15:24:46 +0000

    Updated to use pgp

--- jobs/Philips/MRI/stt.js
+++ jobs/Philips/MRI/stt.js
@@ -1,7 +1,8 @@
+const db = require("../../../utils/db/pg-pool");
+const pgp = require("pg-promise")();
 const { philips_re } = require("../../../parse/parsers");
 const mapDataToSchema = require("../../../persist/map-data-to-schema");
 const { stt_magnet } = require("../../../persist/pg-schemas");
-const bulkInsert = require("../../../persist/queryBuilder");
 const { blankLineTest } = require("../../../util/regExHelpers");
 const generateDateTime = require("../../../processing/date_processing/generateDateTimes");
 const [addLogEvent] = require("../../../utils/logger/log");
@@ -9,6 +10,10 @@ const {
   type: { I, W, E },
   tag: { cal, cat, det },
 } = require("../../../utils/logger/enums");
+
+const {
+  pg_column_sets: pg_cs,
+} = require("../../../utils/db/sql/pg-helpers_hhm");
 
 async function stt_parser(file_config, System) {
   const parsers = file_config.stt_magnet.parsers;
@@ -20,104 +25,123 @@ async function stt_parser(file_config, System) {
     file: file_config,
   };
 
-  await System.getRedisFileSize();
+  try {
+    await addLogEvent(
+      I,
+      System.run_log,
+      "phil_mri_logcurrent",
+      cal,
+      note,
+      null
+    );
 
-  await System.getCurrentFileSize();
+    await System.getRedisFileSize();
 
-  await System.getFileData();
+    await System.getCurrentFileSize();
 
-  if (System.file_data === null) return;
+    await System.getFileData();
 
-  let line_number = 1;
-  for await (const line of System.file_data) {
-    let matches = await line.match(philips_re[parsers[0]]);
+    if (System.file_data === null) return;
 
-    console.log(matches);
+    let line_number = 1;
+    for await (const line of System.file_data) {
+      let matches = await line.match(philips_re[parsers[0]]);
 
-    if (matches === null) {
-      const isNewLine = blankLineTest(line);
-      if (isNewLine) {
+      if (matches === null) {
+        const isNewLine = blankLineTest(line);
+        if (isNewLine) {
+          line_number++;
+          continue;
+        } else {
+          let note = {
+            job_id: System.job_id,
+            sme: System.sme,
+            file: file_config,
+            message: "This is not a blank or new line - Bad Match",
+            line,
+            line_number,
+          };
+          await addLogEvent(I, System.run_log, "stt_parser", cal, note, null);
+          line_number++;
+          continue;
+        }
+      } else if (matches.groups === undefined) continue;
+      else {
         line_number++;
-        continue;
-      } else {
-        let note = {
-          job_id: System.job_id,
-          sme: System.sme,
-          file: file_config,
-          message: "This is not a blank or new line - Bad Match",
-          line,
-          line_number,
-        };
-        await addLogEvent(I, System.run_log, "stt_parser", cal, note, null);
-        line_number++;
-        continue;
+        matches.groups.system_id = System.sme;
+
+        // 23-JUL-2023 03:06:20.81
+        // Add 0 to end of time string for Lexon: 03:06:20.81 = 03:06:20.810
+        matches.groups.host_time += "0";
+
+        const dtObject = await generateDateTime(
+          System.job_id,
+          matches.groups.system_id,
+          "stt_magnet",
+          matches.groups.host_date,
+          matches.groups.host_time
+        );
+
+        // magnet_meu group does not have datetime. Ex: '0114,2022,04,01,00,06,08,17,14,00000,'
+        if (dtObject === null) {
+          let note = {
+            job_id: System.job_id,
+            sme: System.sme,
+            file: file_config,
+            line,
+            match_group: matches.groups,
+            message: "date_time object null",
+          };
+          await addLogEvent(
+            W,
+            System.run_log,
+            "phil_mri_logcurrent",
+            det,
+            note,
+            null
+          );
+          await log("warn", System.job_id, System.sme, "date_time", "FN CALL", {
+            message: "date_time object null",
+            date: matches.groups.host_date,
+            time: matches.groups.host_time,
+            line,
+            line_number,
+          });
+        }
+
+        matches.groups.host_datetime = dtObject;
+
+        data.push(matches.groups);
       }
-    } else if (matches.groups === undefined) continue;
-    else {
-      line_number++;
-      matches.groups.system_id = System.sme;
-
-      // 23-JUL-2023 03:06:20.81
-      // Add 0 to end of time string for Lexon: 03:06:20.81 = 03:06:20.810
-      matches.groups.host_time += "0";
-
-      const dtObject = await generateDateTime(
-        System.job_id,
-        matches.groups.system_id,
-        "stt_magnet",
-        matches.groups.host_date,
-        matches.groups.host_time
-      );
-
-      // magnet_meu group does not have datetime. Ex: '0114,2022,04,01,00,06,08,17,14,00000,'
-      if (dtObject === null) {
-        let note = {
-          job_id: System.job_id,
-          sme: System.sme,
-          file: file_config,
-          line,
-          match_group: matches.groups,
-          message: "date_time object null",
-        };
-        await addLogEvent(W, run_log, "phil_mri_logcurrent", det, note, null);
-        await log("warn", job_id, System.sme, "date_time", "FN CALL", {
-          message: "date_time object null",
-          date: matches.groups.host_date,
-          time: matches.groups.host_time,
-          line,
-          line_number,
-        });
-      }
-
-      matches.groups.host_datetime = dtObject;
-
-      data.push(matches.groups);
     }
-  }
-  console.log(data[99]);
 
-  // Homogenize data to prep for insert to db
-  const mappedData = mapDataToSchema(data, stt_magnet);
-  const dataToArray = mappedData.map(({ ...rest }) => Object.values(rest));
+    // Homogenize data to prep for insert to db
+    const mappedData = mapDataToSchema(data, stt_magnet);
 
-  // ** End Parse
+    // ** End Parse **
 
-  // ** Begin Persist
+    // ** Begin Persist **
 
-  const insertSuccess = await bulkInsert(
-    System.job_id,
-    dataToArray,
-    System.sysConfigData,
-    System.file_config.stt_magnet,
-    System.run_log
-  );
+    const query = pgp.helpers.insert(mappedData, pg_cs.log.philips.stt_magnet);
 
-  // ** End Persist
+    await db.any(query);
 
-  // Update Redis Cache
+    // ** End Persist **
 
-  if (insertSuccess) {
+    // Update Redis Cache
+
     await System.updateRedisFileSize();
+
+  } catch (error) {
+    console.log(error);
+    await addLogEvent(
+      E,
+      System.run_log,
+      "phil_mri_logcurrent",
+      cat,
+      note,
+      error
+    );
   }
 }
 
