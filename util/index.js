b0f1344a473c0b7034d61c743fabfc47ff7fba4d
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2023-12-18 21:25:18 +0000

    Check if file exists to prevent error

---
+++ util/index.js
@@ -0,0 +1,4 @@
+const does_file_exist = require("./does_file_exist");
+const { isFileModified, getLastModifiedTime } = require("./isFileModified");
+
+module.exports = { does_file_exist, isFileModified, getLastModifiedTime };
